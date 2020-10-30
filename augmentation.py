babd2aa0e2f49c9d750625a08dd203590470b616
Shreyank Patel <spatel91@vols.utk.edu>
2020-10-30 17:11:27 -0400

    add more cols to data

--- augmentation.py
+++ augmentation.py
@@ -1,6 +1,7 @@
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
-import pandas as pd 
+import pandas as pd
+from better_profanity import profanity
 
 def sentiment_scores(sentence, neg, neu, pos, compound): 
     sid_obj = SentimentIntensityAnalyzer() 
@@ -16,14 +17,44 @@ if __name__ == "__main__" :
     data = pd.read_csv('data/dataset.csv')
     content = data['content']
     annotation = data['annotation']
+    cuss_word = []
+    f = open('cuss_words.txt','r')
+    for line in f:
+        cuss_word.append(line.strip())
+    
     d1 = pd.DataFrame(data)
     neg = []
     neu = []
     pos = []
     sentence_len = []
     compound = []
+    punctuation_count = []
+    contain_profanity = []
+    num_profanity = []
     for sentence in content:
+        #1 if has profanity, 0 if no profanity
+        count = 0
+        if(profanity.contains_profanity(sentence)):
+            contain_profanity.append(1)
+            for word in sentence.split():
+                if word in cuss_word:
+                    count = count + 1
+            num_profanity.append(count)
+        else:
+            contain_profanity.append(0)
+            num_profanity.append(count)
+     
+        #length of sentence
         sentence_len.append(len(sentence))
+
+        #punctuation ratio
+        count = 0
+        for i in range (0, len(sentence)):
+            if sentence[i] in ('!', "," ,"\'" ,";" ,"\"", ".", "-" ,"?"):
+                count = count + 1
+        punctuation_count.append((count/len(sentence)))
+
+        #vader sentiment
         sentiment_scores(sentence, neg, neu, pos, compound)
     
     d1['sentence_length'] = sentence_len
@@ -31,4 +62,7 @@ if __name__ == "__main__" :
     d1["neu"] = neu
     d1["pos"] = pos
     d1['compound'] = compound
+    d1['punctuation_count'] = punctuation_count
+    d1['contain_profanity'] = contain_profanity
+    d1['num_profanity'] = num_profanity
     d1.to_csv('data/new_dataset.csv', index=True)
