fc0836d20d61bc86c0d9e3a2c0fcb9c2c003ab75
Shreyank Patel <spatel91@vols.utk.edu>
2020-10-30 16:29:37 -0400

    remove empty content and add new columns

--- augmentation.py
+++ augmentation.py
@@ -2,17 +2,33 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 import pandas as pd 
 
-def sentiment_scores(sentence): 
+def sentiment_scores(sentence, neg, neu, pos, compound): 
     sid_obj = SentimentIntensityAnalyzer() 
-  
-    sentiment_dict = sid_obj.polarity_scores(sentence) 
-      
-    print("Overall sentiment dictionary is : ", sentiment_dict) 
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    #print("Overall sentiment dictionary is : ", sentiment_dict) 
+    neg.append(sentiment_dict["neg"])
+    neu.append(sentiment_dict["neu"])
+    pos.append(sentiment_dict["pos"])
+    compound.append(sentiment_dict["compound"])
     
 # Driver code 
 if __name__ == "__main__" : 
     data = pd.read_csv('data/dataset.csv')
     content = data['content']
     annotation = data['annotation']
+    d1 = pd.DataFrame(data)
+    neg = []
+    neu = []
+    pos = []
+    sentence_len = []
+    compound = []
     for sentence in content:
-        sentiment_scores(sentence)
+        sentence_len.append(len(sentence))
+        sentiment_scores(sentence, neg, neu, pos, compound)
+    
+    d1['sentence_length'] = sentence_len
+    d1["neg"] = neg
+    d1["neu"] = neu
+    d1["pos"] = pos
+    d1['compound'] = compound
+    d1.to_csv('data/new_dataset.csv', index=True)
